7dc7e30a0bcb525b9738efb9f5a6ca992659b0a4
meetijha <meetijha@gmail.com>
2025-09-16 16:55:39 +0530

    Java Basics Practice 14thSept

---
+++ src/ex_02_Java_Basics/Lab022_Numeric_Data_Types.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class Lab022_Numeric_Data_Types {
+
+    byte b = 10;
+    short s = 10;
+    int i  = 10;
+    char c = 'A';
+
+    long phone = 976543210L;
+    long phone1 = 976543210l;
+
+    float f = 3.14f;
+    float f2 = 3.14F;
+
+    double d = 3.12123423;
+}
