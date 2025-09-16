6c98ec6869587dcc4e0478aaf9d4cb20a02c35bc
meetijha <meetijha@gmail.com>
2025-09-16 16:56:36 +0530

    Java Literals Practice 14thSept

---
+++ src/ex_03_Literals/Lab035_Non_Primitive_Literal.java
@@ -0,0 +1,8 @@
+package ex_03_Literals;
+
+public class Lab035_Non_Primitive_Literal {
+    public static void main(String[] args) {
+        String name = "Meeti"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+    }
+}
