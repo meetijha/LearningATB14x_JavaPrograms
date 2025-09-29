eb927b3292482c125e5fc0fc93235ccf13358bef
meetijha <meetijha@gmail.com>
2025-09-29 21:05:26 +0530

    If Condition

---
+++ src/ex_08_If_Condition/Lab084_If_ElseIf_Else.java
@@ -0,0 +1,20 @@
+package ex_08_If_Condition;
+
+public class Lab084_If_ElseIf_Else {
+    public static void main(String[] args) {
+
+        int age = 34;
+        if(age > 34){
+            System.out.println("AGE > 34");
+        }else if( age < 34){
+            System.out.println("AGE  < 34");
+        }else{
+            System.out.println("AGE");
+        }
+
+
+
+
+
+    }
+}
